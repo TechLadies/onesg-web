fb8cae5b99f5f76ae8f8f17e7f0b362a0926cebb
Rachel <rachelchng_@hotmail.com>
2020-10-04 10:53:20 +0800

    prettier

--- router.js
+++ router.js
@@ -2,15 +2,16 @@ import { createWebHistory, createRouter } from 'vue-router'
 import store from './store.js'
 
 const authGuard = (to, from, next) => {
-  const loggedIn =!!store.state.user
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const loggedIn = !!store.state.user
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
   if (loggedIn === requiresAuth) {
     next()
   } else if (!loggedIn && requiresAuth) {
     next('/signin')
   } else if (loggedIn && !requiresAuth) {
     next('/dashboard')
-  } else { // should not get here
+  } else {
+    // should not get here
     console.log(loggedIn, requiresAuth)
   }
 }
@@ -18,8 +19,20 @@ const authGuard = (to, from, next) => {
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/dashboard', name: 'Dashboard', component: () => import('/src/pages/Dashboard.vue'), beforeEnter: authGuard, meta: { requiresAuth: true, layout: 'layout-secure' } },
-    { path: '/signin', name: 'SignIn', component: () => import('/src/pages/SignIn.vue'), beforeEnter: authGuard, meta: { requiresAuth: false, layout: 'layout-public' } },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('/src/pages/Dashboard.vue'),
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true, layout: 'layout-secure' },
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: () => import('/src/pages/SignIn.vue'),
+      beforeEnter: authGuard,
+      meta: { requiresAuth: false, layout: 'layout-public' },
+    },
     { path: '/:catchAll(.*)', name: 'catchAll', redirect: { name: 'SignIn' } },
   ],
 })
